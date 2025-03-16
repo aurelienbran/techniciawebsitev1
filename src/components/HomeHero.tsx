7f3472b8da1541c18e91f42d93f9b62c2b25bf55
aurelienbran <aurelien.bran@hotmail.fr>
2025-03-16 20:53:28 +0100

    Modification du HomeHero pour prendre toute la largeur

--- src/components/HomeHero.tsx
+++ src/components/HomeHero.tsx
@@ -10,7 +10,7 @@ export default function HomeHero() {
     <section className="relative min-h-screen flex items-center">
       <div className="animated-bg"></div>
       
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="w-full px-4 sm:px-6 lg:px-8">
         <div className="grid lg:grid-cols-2 gap-12 items-center">
           <motion.div
             initial={{ opacity: 0, y: 20 }}
